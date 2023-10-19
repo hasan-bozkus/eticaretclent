675852e7c5db65b7df33fc6969e8b970c171b825
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-19 14:13:06 +0300

    ders sonu 39: Kullanıcı Girişi (Authentication) işlemleri

--- src/app/uı/components/login/login.component.ts
+++ src/app/uı/components/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/common/models/user.service';
+import { BaseComponent, SpinnerType } from '../../../base/base.component';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent extends BaseComponent implements OnInit {
 
+  constructor(private userService: UserService, spinner: NgxSpinnerService) {
+
+    super(spinner)
+
+  }
+  ngOnInit(): void { }
+
+  async login(userNameOrEmail: string, password: string) {
+    this.showSpinner(SpinnerType.Cog);
+    await this.userService.login(userNameOrEmail, password, () => this.hideSpinner(SpinnerType.Cog));
+
+  }
 }
