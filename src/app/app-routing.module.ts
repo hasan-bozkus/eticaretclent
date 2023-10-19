f4c6ce1cad7d5e92b8c67161b543853683fd4b8c
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-19 11:39:32 +0300

    ders ortası 39: kullanıcı giriş altyapısı hazırlanıyor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "baskets", loadChildren: () => import("./uı/components/baskets/baskets.module").then(module => module.BasketsModule) },
   { path: "products", loadChildren: () => import("./uı/components/products/products.module").then(module => module.ProductsModule) },
-  { path: "register", loadChildren: () => import("./uı/components/register/register.module").then(module => module.RegisterModule) }
+  { path: "register", loadChildren: () => import("./uı/components/register/register.module").then(module => module.RegisterModule) },
+  { path: "login", loadChildren: () => import("./uı/components/login/login.module").then(module => module.LoginModule) }
 ];
 
 @NgModule({
