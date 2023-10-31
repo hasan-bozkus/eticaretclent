bc7d80300c54dcd41ce61481c37f4aec7f996dbd
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-31 11:46:26 +0300

    Http Interceptor ile Global Http Error Handler

--- src/app/services/common/http-error-handler-interceptor.service.ts
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -1,21 +1,57 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, of } from 'rxjs';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../ui/custom-toastr.service';
+import { MessageType } from '../admin/alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private toastrService: CustomToastrService) { }
 
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     console.log("abc");
     return next.handle(req).pipe(catchError(error => {
-      console.log(error);
+      switch (error.status) {
+        case HttpStatusCode.Unauthorized:
+          this.toastrService.message("Bu işlem için yetkiniz bulunmamaktadır.", "Yetkisiz İşlem", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+
+        case HttpStatusCode.InternalServerError:
+          this.toastrService.message("Sunucuya bağlanılamıyor.", "Sunucu Hatası", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+
+        case HttpStatusCode.BadRequest:
+          this.toastrService.message("Geçersiz istek Yaplıdı.", "Geçersiz İstek", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+
+        case HttpStatusCode.NotFound: this.toastrService.message("Sayfa Bulunamadı.", "Sayfa Bulunamadı", {
+          messageType: ToastrMessageType.Warning,
+          position: ToastrPosition.BottomFullWidth
+        });
+          break;
+
+        default: 
+          this.toastrService.message("Beklenmedik bir hata ile karşılaşıldı.", "Hata", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+      }
       return of(error);
     }));
-    }
+  }
 }
